047d64ac3f2800c0d7da047120053c0b9e8028e8
Casey Cannady <cmcannady@users.noreply.github.com>
2020-09-23 17:48:35 -0400

    Add files via upload

---
+++ TailBESFillDBLog.ps1
@@ -0,0 +1,13 @@
+﻿# *** *************************************************************************** ***
+# *** Title         :: TailBESFillDBLog.ps1                                       ***
+# *** Author        :: Casey Cannady (casey.cannady@hcl.com)                      ***
+# *** Company       :: HCL Software - BigFix Professional Services                ***
+# *** Date Created  :: 09/20/2020                                                 ***
+# *** Date Modified :: 09/23/2020                                                 ***
+# *** *************************************************************************** ***
+
+# Declare script constants and variables
+$LogPath = "C:\Program Files (x86)\BigFix Enterprise\BES Server\FillDBData\FillDB.log"
+
+# Execute PowerShell tail option
+Get-Content $LogPath -tail 10 -wait
