99eb03d14ca85c9a9cb06899538f78ec5a112d7d
Casey Cannady <cmcannady@users.noreply.github.com>
2020-09-23 17:48:23 -0400

    Add files via upload

---
+++ TailBESClientDailyLog.ps1
@@ -0,0 +1,14 @@
+﻿# *** *************************************************************************** ***
+# *** Title         :: TailBESClientDailyLog.ps1                                  ***
+# *** Author        :: Casey Cannady (casey.cannady@hcl.com)                      ***
+# *** Company       :: HCL Software - BigFix Professional Services                ***
+# *** Date Created  :: 09/20/2020                                                 ***
+# *** Date Modified :: 09/23/2020                                                 ***
+# *** *************************************************************************** ***
+
+# Declare script constants and variables
+$DateStr = Get-Date -format "yyyyMMdd" #$Date.ToString("yyyyMMdd")
+$LogPath = "C:\Program Files (x86)\BigFix Enterprise\BES Client\__BESData\__Global\Logs\" + $DateStr + ".log"
+
+# Execute PowerShell tail option
+Get-Content $LogPath -tail 10 -wait
